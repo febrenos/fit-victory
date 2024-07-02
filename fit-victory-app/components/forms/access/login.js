c8663d2c8f15dffebcad86936fa52556fc049efd
febreno <felipesugisawa1@gmail.com>
2024-07-02 19:51:00 -0300

    ajuste versao expo e teclado em cima do input

--- fit-victory-app/components/forms/access/login.js
+++ fit-victory-app/components/forms/access/login.js
@@ -1,70 +1,70 @@
-import { useEffect, useState } from 'react';
-import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useEffect, useState } from "react"
+import { Image, KeyboardAvoidingView, Platform, Text, TextInput, TouchableOpacity, View } from "react-native"
+import { useNavigation } from "@react-navigation/native"
 
-import { ButtonPrimary } from '../../../components/buttons/primary/buttonPrimary.js';
-import Logo from '../../../assets/img/logo.png';
-import {colors, globalStyles} from '../../../styles.js';
-import styles from './styles';
-import useAuthController from '../../../controllers/authController.js';
+import { ButtonPrimary } from "../../../components/buttons/primary/buttonPrimary.js"
+import Logo from "../../../assets/img/logo.png"
+import { colors, globalStyles } from "../../../styles.js"
+import styles from "./styles"
+import useAuthController from "../../../controllers/authController.js"
 
 export const Login = ({ onLogin }) => {
-    const [users, setUsers] = useState([]);
-    const [email, setEmail] = useState("fe@fiap.com");
-    const [password, setPassword] = useState("teste123");
-    const navigation = useNavigation();
-    const authController = useAuthController();
+  const [users, setUsers] = useState([])
+  const [email, setEmail] = useState("fe@fiap.com")
+  const [password, setPassword] = useState("teste123")
+  const navigation = useNavigation()
+  const authController = useAuthController()
 
-    const handleLogin = async () => {
-        try {
-          await authController.login({
-            "login": email,
-            "password": password
-          });
-        } catch (error) {
-          console.error('Error during registration:', error);
-        }
-    };
+  const handleLogin = async () => {
+    try {
+      await authController.login({
+        login: email,
+        password: password,
+      })
+    } catch (error) {
+      console.error("Error during registration:", error)
+    }
+  }
 
-
-    return (
-        <View style={globalStyles.containerPage}>
-            <View style={styles.center}>
-                <Text style={ styles.title }>Login</Text>
-                <View style={ globalStyles.form }>
-                    <View>
-                        <Image style={styles.logo} source={ Logo }/>
-                    </View>
-                    <View>
-                        <View style={{marginBottom: 10}}>
-                            <TextInput 
-                                placeholder={"E-mail"} 
-                                value={email} 
-                                onChangeText={setEmail} 
-                                placeholderTextColor={colors.lightGray} 
-                                style={globalStyles.textInput}
-                            />
-                            <TextInput 
-                                placeholder={"Senha"} 
-                                secureTextEntry={true}
-                                value={password} 
-                                onChangeText={setPassword} 
-                                placeholderTextColor={colors.lightGray} 
-                                style={globalStyles.textInput}
-                            />
-                        </View>
-                        <ButtonPrimary text="Login" onPress={handleLogin} />
-                        <View style={styles.box}>
-                            <Text style={globalStyles.text}>Ainda não tem uma conta?</Text>
-                            <TouchableOpacity onPress={()=> navigation.navigate("Register")}>
-                                <Text style={globalStyles.textBold}>
-                                    Cadastre-se aqui!
-                                </Text>
-                            </TouchableOpacity>
-                        </View>
-                    </View>
-                </View>
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+      style={globalStyles.containerPage}
+    >
+      <View style={styles.center}>
+        <Text style={styles.title}>Login</Text>
+        <View style={globalStyles.form}>
+          <View>
+            <Image style={styles.logo} source={Logo} />
+          </View>
+          <View>
+            <View style={{ marginBottom: 10 }}>
+              <TextInput
+                placeholder={"E-mail"}
+                value={email}
+                onChangeText={setEmail}
+                placeholderTextColor={colors.lightGray}
+                style={globalStyles.textInput}
+              />
+              <TextInput
+                placeholder={"Senha"}
+                secureTextEntry={true}
+                value={password}
+                onChangeText={setPassword}
+                placeholderTextColor={colors.lightGray}
+                style={globalStyles.textInput}
+              />
             </View>
+            <ButtonPrimary text="Login" onPress={handleLogin} />
+            <View style={styles.box}>
+              <Text style={globalStyles.text}>Ainda não tem uma conta?</Text>
+              <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+                <Text style={globalStyles.textBold}>Cadastre-se aqui!</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
-    )
+      </View>
+    </KeyboardAvoidingView>
+  )
 }
--- fit-victory-app/components/forms/access/register.js
+++ fit-victory-app/components/forms/access/register.js
@@ -1,81 +1,92 @@
-import { useState } from 'react';
-import { Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { useState } from "react"
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native"
+import { useNavigation } from "@react-navigation/native"
 
-import Logo from '../../../assets/img/logo.png';
-import { ButtonPrimary } from '../../../components/buttons/primary/buttonPrimary.js';
-import {colors, globalStyles} from '../../../styles.js';
-import styles from './styles';
-import useAuthController from '../../../controllers/authController.js';
+import Logo from "../../../assets/img/logo.png"
+import { ButtonPrimary } from "../../../components/buttons/primary/buttonPrimary.js"
+import { colors, globalStyles } from "../../../styles.js"
+import styles from "./styles"
+import useAuthController from "../../../controllers/authController.js"
 
 export const Register = () => {
-    const [nickname, setNickname] = useState();
-    const [name, setName] = useState();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
-    const navigation = useNavigation();
-    const authController = useAuthController();
+  const [nickname, setNickname] = useState()
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+  const [password, setPassword] = useState()
+  const navigation = useNavigation()
+  const authController = useAuthController()
 
-    const handleRegister = async () => {
-        try {
-          await authController.register({
-            "nickname": nickname,
-            "name": name,
-            "email": email,
-            "password": password,
-          });
-        } catch (error) {
-          console.error('Error during registration:', error);
-        }
-    };
+  const handleRegister = async () => {
+    try {
+      await authController.register({
+        nickname: nickname,
+        name: name,
+        email: email,
+        password: password,
+      })
+    } catch (error) {
+      console.error("Error during registration:", error)
+    }
+  }
 
-    return (
-        <View style={globalStyles.containerPage}>
-            <View style={styles.center}>
-                <Text style={ styles.title }>Registro</Text>
-                <View style={ globalStyles.form }>
-                    <View>
-                        <Image style={styles.logo} source={ Logo }/>
-                    </View>
-                    <View style={{marginBottom: 10}}>
-                        <TextInput 
-                            placeholder={"nome de usuário"} 
-                            value={nickname} 
-                            onChangeText={setNickname} 
-                            placeholderTextColor={colors.text} 
-                            style={globalStyles.textInput}
-                        />
-                        <TextInput 
-                            placeholder={"Nome"} 
-                            value={name} onChangeText={setName} 
-                            placeholderTextColor={colors.text} 
-                            style={globalStyles.textInput}
-                        />
-                        <TextInput 
-                            placeholder={"E-mail"} 
-                            value={email} onChangeText={setEmail} 
-                            placeholderTextColor={colors.text} 
-                            style={globalStyles.textInput}
-                        />
-                        <TextInput 
-                            placeholder={"Senha"} 
-                            value={password} 
-                            onChangeText={setPassword} 
-                            placeholderTextColor={colors.text} 
-                            style={globalStyles.textInput}
-                        />
-                    </View>
-                    <ButtonPrimary text="Registrar" onPress={() => handleRegister()} />
-                    <View style={styles.box}>
-                        <Text style={globalStyles.text}>Já possui cadastro?</Text>
-                        <TouchableOpacity onPress={()=> navigation.navigate("Login")}>
-                            <Text style={globalStyles.textBold}>
-                                Faça o Login
-                            </Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </View>
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS == "ios" ? "padding" : "height"}
+      style={globalStyles.containerPage}
+    >
+      <View style={styles.center}>
+        <Text style={styles.title}>Registro</Text>
+        <View style={globalStyles.form}>
+          <View>
+            <Image style={styles.logo} source={Logo} />
+          </View>
+          <View style={{ marginBottom: 10 }}>
+            <TextInput
+              placeholder={"nome de usuário"}
+              value={nickname}
+              onChangeText={setNickname}
+              placeholderTextColor={colors.text}
+              style={globalStyles.textInput}
+            />
+            <TextInput
+              placeholder={"Nome"}
+              value={name}
+              onChangeText={setName}
+              placeholderTextColor={colors.text}
+              style={globalStyles.textInput}
+            />
+            <TextInput
+              placeholder={"E-mail"}
+              value={email}
+              onChangeText={setEmail}
+              placeholderTextColor={colors.text}
+              style={globalStyles.textInput}
+            />
+            <TextInput
+              placeholder={"Senha"}
+              value={password}
+              onChangeText={setPassword}
+              placeholderTextColor={colors.text}
+              style={globalStyles.textInput}
+            />
+          </View>
+          <ButtonPrimary text="Registrar" onPress={() => handleRegister()} />
+          <View style={styles.box}>
+            <Text style={globalStyles.text}>Já possui cadastro?</Text>
+            <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+              <Text style={globalStyles.textBold}>Faça o Login</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-    )
+      </View>
+    </KeyboardAvoidingView>
+  )
 }
